edbf2d48f6f19878bdda8c6eda00d2739db9c89e
oceanocely <oliveiramarcely@id.uff.br>
2026-04-09 13:53:57 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,19 @@
+## Script referente à atividade 4 da disciplina Prática Estatística ##
+
+# Pacote #
+
+library(ggplot2) 
+
+# 1. Leitura e estrutura dos dados #
+
+data = read.csv("imoveis.csv", header = T, sep = ";")
+str(data)
+
+# 2. Gráficos #
+
+# 3. Medidas #
+
+
+
+
+
